46f787c5e113fb1b52b3dc33bb8336596ba69840
CherOx <J.CherOks@gmail.com>
2013-04-19 18:14:24 +0300

    Fourth commit

--- src/main/java/karaoke/client/view/WorkView.java
+++ src/main/java/karaoke/client/view/WorkView.java
@@ -168,31 +168,31 @@ public class WorkView extends Composite {
 //        }
 //    }
 
-//    public native void setSelectionRange(Element elem, int pos, int length) /*-{
-//        try {
-//            var selection = null, range2 = null;
-//            var iframeWindow = elem.contentWindow;
-//            var iframeDocument = iframeWindow.document;
-//
-//            selection = iframeWindow.getSelection();
-//            range2 = selection.getRangeAt(0);
-//
-//            //create new range
-//            var range = iframeDocument.createRange();
-//            range.setStart(selection.anchorNode, pos);
-//            range.setEnd(selection.anchorNode, pos+length);
-//
-//            //remove the old range and add the newly created range
-//            if (selection.removeRange) { // Firefox, Opera, IE after version 9
-//                selection.removeRange(range2);
-//            } else {
-//                if (selection.removeAllRanges) { // Safari, Google Chrome
-//                    selection.removeAllRanges();
-//                }
-//            }
-//            selection.addRange(range);
-//        } catch (e) {
-//            $wnd.alert(e);
-//        }
-//    }-*/;
+    public native void setSelectionRange(Element elem, int pos, int length) /*-{
+        try {
+            var selection = null, range2 = null;
+            var iframeWindow = elem.contentWindow;
+            var iframeDocument = iframeWindow.document;
+
+            selection = iframeWindow.getSelection();
+            range2 = selection.getRangeAt(0);
+
+            //create new range
+            var range = iframeDocument.createRange();
+            range.setStart(selection.anchorNode, pos);
+            range.setEnd(selection.anchorNode, pos+length);
+
+            //remove the old range and add the newly created range
+            if (selection.removeRange) { // Firefox, Opera, IE after version 9
+                selection.removeRange(range2);
+            } else {
+                if (selection.removeAllRanges) { // Safari, Google Chrome
+                    selection.removeAllRanges();
+                }
+            }
+            selection.addRange(range);
+        } catch (e) {
+            $wnd.alert(e);
+        }
+    }-*/;
 }
